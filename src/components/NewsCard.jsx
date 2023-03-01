46ca759af0c718f83af4f5e4678f0f0746ef45ee
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2023-03-01 21:10:25 +0530

    Added Redirected URL

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,17 +1,19 @@
 const NewsCard = ({ article }) => {
 	return (
-		<div className="p-2 h-20 bg-white flex gap-1 justify-between border overflow-hidden">
-			<div className="flex flex-col">
-				<h3 className="text-xs font-medium">{article.title}</h3>
+		<a href={article?.readMoreUrl} target="_blank">
+			<div className="p-2 h-20 bg-white flex gap-1 justify-between border overflow-hidden">
+				<div className="flex flex-col">
+					<h3 className="text-xs font-medium">{article.title}</h3>
+				</div>
+				<div className="bg-gray-200 transition duration-400">
+					<img
+						src={article.imageUrl}
+						alt={article.title}
+						className="w-36 transition duration-300 hover:opacity-75 object-cover"
+					/>
+				</div>
 			</div>
-			<div className="bg-gray-200 transition duration-400">
-				<img
-					src={article.imageUrl}
-					alt={article.title}
-					className="w-36 transition duration-300 hover:opacity-75 object-cover"
-				/>
-			</div>
-		</div>
+		</a>
 	);
 };
 
